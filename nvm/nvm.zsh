a3ee6d21060f08aabda40733df87eefd0d89c6fb
Omid Haqbin <omid.haqbin@gmail.com>
2023-03-23 10:07:28 +0100

    add old nvm stuff

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -3,4 +3,9 @@ if [ -e "$(brew --prefix nvm)/nvm.sh" ]; then
   source $(brew --prefix nvm)/nvm.sh
 fi
 
+# from my old previous .zshrc
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm use node
